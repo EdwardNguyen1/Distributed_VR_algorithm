66e458e74f32e176510d3d8ce8880f8b646ee56a
ybc1991 <bichengying@gmail.com>
2017-05-15 14:42:59 -0700

    modify read_data

--- read_data.py
+++ read_data.py
@@ -8,7 +8,7 @@ import os
 
 def split_data(X, y, N_agent, even=True):
     '''
-    split the data to N_agent chunks. Will remove the remainder data if all data didn't split evenly
+    split the data to N_agent chunks. Will remove the remainder data if all data can not split evenly
     '''
     if even:
         N_data_dist_prob = np.ones(N_agent) / N_agent
@@ -31,8 +31,8 @@ def split_data(X, y, N_agent, even=True):
 
     return X, y, N_data_boundary, N_data_agent_mean
 
-def read_cov():
-    covtype=datasets.fetch_covtype(data_home='./real_data')
+def read_cov(**kwargs):
+    covtype=datasets.fetch_covtype(data_home='../data')
     # extract only two label data 
     mask = np.in1d(covtype.target, [1,2])
     covtype.data = covtype.data[mask]
@@ -41,16 +41,22 @@ def read_cov():
     std = sklearn.preprocessing.StandardScaler()
     norm = sklearn.preprocessing.Normalizer()
     covtype.data = norm.fit_transform(covtype.data)
-    X_train, X_test, y_train, y_test = train_test_split(covtype.data, covtype.target, test_size=0.6)
+    X_train, X_test, y_train, y_test = \
+        train_test_split(covtype.data, covtype.target, test_size=kwargs.get('test_size', 0.6))
     y_train[y_train==2] = -1
     y_test[y_test==2] = -1
 
     return X_train, y_train[:, np.newaxis]
 
-def read_mnist():
+def read_mnist(**kwargs):
     import tensorflow.examples.tutorials.mnist.input_data as input_data
-    file_loc=os.path.abspath('C:/Users/biche/OneDrive/Documents/Python Scripts/random_reshuffle/MNIST_data') 
-    ds = input_data.read_data_sets(file_loc, one_hot=False)
+    try:
+        # replace it by your own path
+        file_loc=os.path.abspath('C:/Users/biche/OneDrive/Documents/Python Scripts/random_reshuffle/MNIST_data') 
+        ds = input_data.read_data_sets(file_loc, one_hot=False)
+    except:
+        ds = input_data.read_data_sets('../data', one_hot=False)
+
     X = ds.train.images
     y = ds.train.labels
 
@@ -71,9 +77,13 @@ def read_cifar():
             dict = pickle.load(fo, encoding='bytes')
         return dict
 
-    file_loc=os.path.abspath('C:/Users/biche/OneDrive/Documents/Python Scripts/random_reshuffle/real_data/cifar-10') 
-    cifar_data = unpickle(os.path.join(file_loc,'data_batch_1'))
-    cifar_data_test = unpickle(os.path.join(file_loc,'test_batch'))
+    try:
+        file_loc=os.path.abspath('C:/Users/biche/OneDrive/Documents/Python Scripts/random_reshuffle/real_data/cifar-10') 
+        cifar_data = unpickle(os.path.join(file_loc,'data_batch_1'))
+        cifar_data_test = unpickle(os.path.join(file_loc,'test_batch'))
+    except:
+        print ('did not find the cifar document')
+        return None
 
     X_cifar_train = cifar_data[b'data']
     y_cifar_train = np.array(cifar_data[b'labels'])
@@ -90,8 +100,10 @@ def read_cifar():
 
     normalizer = sklearn.preprocessing.Normalizer()
     std = sklearn.preprocessing.StandardScaler()
+
     X_cifar_train = normalizer.fit_transform(X_cifar_train)
     X_cifar_test = normalizer.transform(X_cifar_test)
+    
     y_cifar_train[y_cifar_train==0] = -1
     y_cifar_test[y_cifar_test==0] = -1
 
