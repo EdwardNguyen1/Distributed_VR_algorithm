2e6f6d1e32d53ab20988d236a00651a683cbef7f
ybc1991 <bichengying@gmail.com>
2017-05-25 19:09:50 -0700

    add mnist zmq main function

--- read_data.py
+++ read_data.py
@@ -79,15 +79,14 @@ def read_mnist(datatype ="binary", **kwargs):
         mask_label = kwargs.get('mask_label', [0, 1, 2, 3, 4])
         
         ds_oh = input_data.read_data_sets(file_loc, one_hot=True)
-        ds = input_data.read_data_sets(file_loc, one_hot=False)
 
         X = ds_oh.train.images
         Y = ds_oh.train.labels
-        y = ds.train.labels
+        y = np.argmax(Y, axis = 1)
 
         mask = np.in1d(y, mask_label)
         normalizer = sklearn.preprocessing.Normalizer()
-        X= X[mask]
+        X = X[mask]
         X_train = normalizer.fit_transform(X)
         Y_train = Y[mask]
 
